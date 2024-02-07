c52cea23a8fade5d18f1867dc09ebd2e50968158
Izet Delibasic <izetdelibasic55@gmail.com>
2024-02-07 23:22:54 +0100

    Adding search bar on landing page of dashboard

--- src/pages/master/dashboardPage.vue
+++ src/pages/master/dashboardPage.vue
@@ -42,10 +42,13 @@
         </div>
         <div class="w-full h-full bg-gray-400">
             <div class="h-[50px] bg-gray-200 flex items-center shadow-sm px-[20px] w-full py-[10px] z-10 border-b">
-                <font-awesome-icon icon="bars" @click="toggleSideBar" />
+                <font-awesome-icon icon="bars" @click="toggleSideBar" class="mr-[20px]"/>
+                <div class="relative">
+                    <input type="text" class="p-2 pl-8 rounded border border-gray-200 bg-gray-200 focus:bg-white focus:outline-none focus:ring-2 focus:ring-gray-800 focus:border-transparent" placeholder="What you thinking now?" value="" />
+                </div>
             </div>
             <div class="h-[calc(100vh-50px)] bg-white">
-                Main
+                
             </div>
         </div>
     </div>
