89c8616aa6357f9b9b4ad6a8103c1dfe3bd72283
Izet Delibasic <izetdelibasic55@gmail.com>
2024-02-13 23:15:00 +0100

    Importing api files and trying to fetch

--- src/pages/homePage.vue
+++ src/pages/homePage.vue
@@ -1,13 +1,49 @@
-<template lang="">
+<template>
     <div>
-        Home
+      <h1>Home</h1>
+      <div v-if="loading">Loading...</div>
+      <div v-else>
+        <div v-for="team in teams" :key="team.id">
+          <h2>{{ team.name }}</h2>
+        </div>
+      </div>
     </div>
-</template>
-<script>
-export default {
-    
-}
-</script>
-<style lang="">
-    
-</style>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        teams: [],
+        loading: true
+      };
+    },
+    async mounted() {
+      try {
+        const response = await axios.request({
+          method: 'GET',
+          url: 'https://api-basketball.p.rapidapi.com/teams',
+          params: {
+            league: '12',
+            season: '2019-2020'
+          },
+          headers: {
+            'X-RapidAPI-Key': '60efaf8409msh8cea9ef9d606876p1b2ea0jsnc0c8807fac58',
+            'X-RapidAPI-Host': 'api-basketball.p.rapidapi.com'
+          }
+        });
+        this.teams = response.data;
+        this.loading = false;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  /* Stilizirajte prema potrebama */
+  </style>
+  
