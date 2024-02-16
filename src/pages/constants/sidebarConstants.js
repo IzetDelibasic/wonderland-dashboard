91638e5b1c152257d2995bd6e95ccd683c590384
Izet Delibasic <izetdelibasic55@gmail.com>
2024-02-16 23:30:33 +0100

    Creating constants folder and imporing sidebar constants for links

---
+++ src/pages/constants/sidebarConstants.js
@@ -0,0 +1,52 @@
+export const sidebarConstant = [
+    {
+        title: 'Home',
+        items: [
+          {
+            name: 'Home',
+            href: '/',
+            icon: 'fa-solid fa-house-user',
+          },
+        ],
+      },
+      {
+        title: 'Profile',
+        items: [
+          {
+            name: 'Profile',
+            href: '/profile',
+            icon: 'users',
+          },
+        ],
+      },
+      {
+        title: 'Messages',
+        items: [
+          {
+            name: 'Messages',
+            href: '/messages',
+            icon: 'message',
+          },
+        ],
+      },
+      {
+        title: 'Download',
+        items: [
+          {
+            name: 'Download',
+            href: '/download',
+            icon: 'cloud-arrow-down',
+          },
+        ],
+      },{
+        title: 'Settings',
+        items: [
+          {
+            name: 'Settings',
+            href: '/settings',
+            icon: 'sliders',
+          },
+        ],
+      },
+
+]
