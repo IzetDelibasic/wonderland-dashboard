f785c6eb9a1e9ed70143533897ff39b10d8755cd
Izet Delibasic <izetdelibasic55@gmail.com>
2024-02-27 02:07:13 +0100

    Adding images and constant for character history

---
+++ src/pages/constants/characterHistoryConstants.js
@@ -0,0 +1,44 @@
+export const charactedHistoryConstant = [
+    {
+        character: 'Mickey Mouse',
+        items: [
+          {
+            name: 'Mickey Mouse',
+            history: 'Mickey was created in 1928 by Walt Disney and Ub Iwerks. He made his first appearance in the animated short film "Steamboat Willie," which premiered on November 18, 1928. Mickey is known for his cheerful character, red shorts, large yellow shoes, and white gloves. He is the symbol of the Disney brand and often appears as the hero in various adventures.',
+            image: '@/images/mickeyMouseImage.webp',
+          },
+        ],
+        character: 'Simba',
+        items: [
+          {
+            name: 'Simba',
+            history: 'Simba is the main character in Disney`s animated film "The Lion King" from 1994, directed by Roger Allers and Rob Minkoff. Born as the son of King Mufasa, Simba grows up to become the future king of the savanna. However, after the death of his father and manipulations by his uncle Scar, Simba leaves his kingdom, only to later return to claim his rightful place as king.',
+            image: '@/images/simbaImage.webp',
+          },
+        ],
+        character: 'Ariel',
+        items: [
+          {
+            name: 'Ariel',
+            history: 'Ariel is the main protagonist of Disney`s animated film "The Little Mermaid" from 1989. She is a curious mermaid who dreams of life on land. Risking everything, including her voice, Ariel embarks on an adventure to win the heart of a prince and fulfill her dream of living outside the sea.',
+            image: '@/images/arielImage.webp',
+          },
+        ],
+        character: 'Buzz Lightyear',
+        items: [
+          {
+            name: 'Buzz Lightyear',
+            history: 'Buzz Lightyear is one of the main characters in Disney`s animated film "Toy Story" from 1995, directed by John Lasseter. He is a high-tech toy who arrives as a new addition to Andy`s toy collection, causing envy and insecurity in Andy`s first favorite toy, Woody. Buzz is brave and self-confident, but he also undergoes a process of discovering his true nature as a toy.',
+            image: '@/images/buzzImage.webp',
+          },
+        ],
+        character: 'Elsa',
+        items: [
+          {
+            name: 'Elsa',
+            history: 'Elsa is the main character in Disney`s animated film "Frozen" from 2013, directed by Chris Buck and Jennifer Lee. She is the princess of the kingdom of Arendelle with powers over ice and snow. After inadvertently causing a winter storm, Elsa withdraws into solitude to learn to control her powers. However, her younger sister Anna decides to embark on an adventure to find her and restore harmony to their kingdom.',
+            image: '@/images/elsaImage.webp',
+          },
+        ],
+    }
+]
