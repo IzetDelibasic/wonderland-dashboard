3a20393033fe2be410d83b788c577773e5ab7c9e
Izet Delibasic <izetdelibasic55@gmail.com>
2024-02-28 01:38:55 +0100

    Finishing card style on History page

--- src/pages/HistoryPage.vue
+++ src/pages/HistoryPage.vue
@@ -8,13 +8,13 @@
         </div>
       </div>
     </div>
-    <div class="w-[40%] flex flex-col justify-center items-center mx-auto mt-[3rem] bg-bluePurple bg-opacity-90 p-[5rem]">
+    <div class="w-[60%] flex flex-col justify-center items-center mx-auto mt-[3rem] bg-white bg-opacity-80 rounded-[30px] rounded-bl-none rounded-tr-none hover:bg-opacity-45 hover:rounded-[30px] ease-in-out duration-300 p-[5rem]">
       <div class="xl:h-[25rem]">
         <div v-if="currentCharacter" class="">
           <div v-for="item in currentCharacter.items" :key="item.name" class="flex flex-col justify-center items-center text-center">
-            <h3>{{ item.name }}</h3>
-            <img :src="requireImage(item.image)" :alt="item.name" class="w-[15rem] h-[15rem]"/>
-            <p class="w-[50%] py-[0.5rem]">{{ item.history }}</p>
+            <h3 class="text-[26px] pb-[1rem] font-montserrat font-medium">{{ item.name }}</h3>
+            <img :src="requireImage(item.image)" :alt="item.name" class="w-[15rem] h-[15rem] rounded-[25px]"/>
+            <p class="w-[50%] py-[0.5rem] font-montserrat lg:text-[18px] font-medium">{{ item.history }}</p>
           </div>
         </div>
         <div v-else>
@@ -22,8 +22,8 @@
         </div>
       </div>
       <div class="flex flex-row justify-between w-[40%] pt-[10rem]">
-        <CustomButton @click="previousCharacter" :title="buttonOneText" class="bg-bluePurple hover:bg-black text-white font-medium rounded font-workSans ease-out duration-300 w-[40%]"/>
-        <CustomButton @click="nextCharacter" :title="buttonTwoText" class="bg-bluePurple hover:bg-black text-white font-medium rounded font-workSans ease-out duration-300 w-[40%]"/>
+        <CustomButton @click="previousCharacter" :title="buttonOneText" class="bg-gray-900 hover:bg-bluePurple text-white font-medium rounded font-workSans ease-out duration-300 w-[40%]"/>
+        <CustomButton @click="nextCharacter" :title="buttonTwoText" class="bg-gray-900 hover:bg-bluePurple text-white font-medium rounded font-workSans ease-out duration-300 w-[40%]"/>
       </div>
     </div>
   </div>
