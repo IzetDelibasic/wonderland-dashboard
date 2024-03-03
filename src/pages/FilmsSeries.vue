b8ecb377c4c0d0723f189c878e2b4fb4d219920c
Izet Delibasic <izetdelibasic55@gmail.com>
2024-03-03 23:36:06 +0100

    Creating style for FilmsSeries page

--- src/pages/FilmsSeries.vue
+++ src/pages/FilmsSeries.vue
@@ -1,13 +1,29 @@
-<template lang="">
-    <div>
-        FilmsSeries
+<template>
+    <div class="min-h-screen bg-cover bg-gray-200 text-center " :style="{ backgroundImage: 'url(' + require('@/images/dashboardBackground.webp') + ')' }">
+      <div class="w-full bg-gray-900 text-white">
+        <div class="h-[50px] px-4 flex items-center justify-between">
+          <h3 class="font-bold font-montserrat lg:text-[1.5rem]">Wonderland Dashboard</h3>
+          <font-awesome-icon icon="fa-solid fa-house-user" class="cursor-pointer" @click="goToStartPage"/>
+        </div>
+      </div>
     </div>
-</template>
-<script>
-export default {
-    
-}
-</script>
-<style lang="">
-    
-</style>
+  </template>
+  
+  <script>
+  
+  export default {
+    data() {
+
+    },
+    methods: {
+      goToStartPage() {
+        this.$router.push('/');
+      }
+    },
+  };
+  </script>
+  
+  <style>
+  
+  </style>
+  
