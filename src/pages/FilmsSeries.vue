11f00cd7f38df4f31e6735b5bd1b8f0e4375db1b
Izet Delibasic <izetdelibasic55@gmail.com>
2024-03-06 00:27:11 +0100

    Importing Button component and fixing resposnive for page

--- src/pages/FilmsSeries.vue
+++ src/pages/FilmsSeries.vue
@@ -8,21 +8,21 @@
     </div>
 
     <div class="mt-8 mx-auto w-[80%] lg:w-[30rem]">
-      <input v-model="movieTitle" type="text" class="border border-gray-300 rounded px-[1rem] py-[0.5rem] w-full" placeholder="Enter movie title">
-      <button @click="searchMovie" class="bg-bluePurple hover:bg-gray-600 ease-out duration-300 text-white font-bold py-[0.5rem] px-[1rem] rounded mt-[0.5rem]">Search Movie</button>
+      <input v-model="movieTitle" type="text" class="border border-gray-300 rounded px-[1rem] py-[0.5rem] mb-[1rem] w-full" placeholder="Enter movie title">
+      <CustomButton  :title="searchTitle" @click="searchMovie" class="bg-bluePurple hover:bg-gray-900 text-white font-medium rounded font-montserrat ease-out duration-300 lg:w-[45%] mb-[1rem] lg:mb-0 mx-auto"/>
     </div>
 
     <!-- Display movie information -->
     <div></div>
-    <div v-if="movieInfo" class="mt-[5rem] mx-auto lg:w-[50%] bg-white bg-opacity-85 font-montserrat p-[3rem] rounded-[1rem] rounded-br-none">
+    <div v-if="movieInfo" class="mt-[5rem] mx-auto lg:w-[60%] w-[80%] bg-white bg-opacity-85 font-montserrat p-[3rem] rounded-[1rem] rounded-br-none">
       <h4 class="font-bold text-[1.25rem]">{{ movieInfo.Title }}</h4>
       <p>{{ movieInfo.Plot }}</p>
-      <div class="flex mx-auto lg:w-[60%] w-[80%] justify-between">
-        <div class="flex flex-col font-montserrat font-bold text-start">
-          <p>Director:</p>
-          <p>Actors:</p>
-          <p>Year:</p>
-          <p>Rating:</p>
+      <div class="lg:flex mx-auto lg:w-[90%] xl:w-[80%] lg:justify-center justify-between hidden">
+        <div class="flex flex-col font-montserrat font-bold text-start mr-[0.2rem]">
+          <p>Director: </p>
+          <p>Actors: </p>
+          <p>Year: </p>
+          <p>Rating: </p>
         </div>
         <div class="flex flex-col text-start font-montserrat">
           <p>{{ movieInfo.Director }}</p>
@@ -31,17 +31,29 @@
           <p>{{ movieInfo.imdbRating }}</p>
         </div>
       </div>
+      <div class="flex flex-col font-montserrat text-center lg:hidden">
+        <p><strong>Director: </strong>{{ movieInfo.Director }}</p>
+        <p><strong>Actors: </strong>{{ movieInfo.Actors }}</p>
+        <p><strong>Year: </strong>{{ movieInfo.Year }}</p>
+        <p><strong>Rating: </strong>{{ movieInfo.imdbRating }}</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import CustomButton from '@/components/CustomButton.vue'
+
 export default {
   data() {
     return {
       movieTitle: "",
-      movieInfo: null 
+      movieInfo: null,
+      searchTitle: "Search your movie",
     };
+  },
+  components: {
+    CustomButton,
   },
   methods: {
     goToStartPage() {
