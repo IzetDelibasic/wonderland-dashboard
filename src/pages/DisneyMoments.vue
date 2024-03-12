72632a8efd466a06ea82153d555aae7809477f8b
Izet Delibasic <izetdelibasic55@gmail.com>
2024-03-12 01:08:35 +0100

    Importing first style for Disney Moments component

--- src/pages/DisneyMoments.vue
+++ src/pages/DisneyMoments.vue
@@ -1,13 +1,21 @@
 <template lang="">
-    <div>
-        DisneyMoments
+    <div :style="{ backgroundImage: 'url(' + require('@/images/dashboardBackground.webp') + ')', backgroundSize: 'cover' }" class="min-h-screen">
+      <div class="w-full h-full text-center text-white">
+        <div class="h-[3rem] bg-gray-900">
+          <div class="h-full px-[2rem] flex items-center justify-between">
+            <h3 class="font-bold font-montserrat lg:text-[1.5rem] cursor-default">Wonderland Dashboard</h3>
+            <font-awesome-icon icon="fa-solid fa-house-user" class="cursor-pointer text-xl" @click="goToStartPage"/>
+          </div>
+        </div>
+      </div>
     </div>
-</template>
-<script>
-export default {
-    
-}
-</script>
-<style lang="">
-    
-</style>
+  </template>
+  
+  <script>
+  export default {
+  };
+  </script>
+  
+  <style lang="">
+  </style>
+  
