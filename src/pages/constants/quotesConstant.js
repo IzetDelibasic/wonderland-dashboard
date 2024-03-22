76adf72e002ffdba20f87c2c62a0dd9fac77378a
Izet Delibasic <izetdelibasiic@gmail.com>
2024-03-22 16:27:19 +0100

    Creating constant for quotes

---
+++ src/pages/constants/quotesConstant.js
@@ -0,0 +1,91 @@
+export const disneyQuotes = [
+  {
+    author: "Rapunzel (Tangled)",
+    quote: "Venture outside your comfort zone. The rewards are worth it.",
+  },
+  {
+    author: "Peter Pan (Peter Pan)",
+    quote: "All it takes is faith and trust.",
+  },
+  {
+    author: "The Emperor (Mulan)",
+    quote:
+      "The flower that blooms in adversity is the most rare and beautiful of all.",
+  },
+  {
+    author: "Dumbo (Dumbo)",
+    quote: "Don't just fly, soar.",
+  },
+  {
+    author: "Mary Poppins (Mary Poppins)",
+    quote: "In every job that must be done, there is an element of fun.",
+  },
+  {
+    author: "Laverne (The Hunchback of Notre Dame)",
+    quote:
+      "Life's not a spectator sport. If watchin' is all you're gonna do, then you're gonna watch your life go by without ya.",
+  },
+  {
+    author: "Jack Sparrow (Pirates of the Caribbean)",
+    quote:
+      "The problem is not the problem. The problem is your attitude about the problem.",
+  },
+  {
+    author: "Mulan (Princess Stories)",
+    quote: "Believe you can, then you will.",
+  },
+  {
+    author: "Quasimodo (The Hunchback of Notre Dame)",
+    quote: "Today is a good day to try.",
+  },
+  {
+    author: "The Cheshire Cat (Alice in Wonderland)",
+    quote:
+      "If you don't know where you want to go, then it doesn't matter which path you take.",
+  },
+  {
+    author: "Beret Girl (An Extremely Goofy Movie)",
+    quote:
+      "Admit defeat, and defeat will surely admit you into permanent custody.",
+  },
+  {
+    author: "Rafiki (The Lion King)",
+    quote:
+      "Oh yes, the past can hurt. But the way I see it, you can either run from it or learn from it.",
+  },
+  {
+    author: "Winnie the Pooh (Pooh's Most Grand Adventure)",
+    quote:
+      "You're braver than you believe, and stronger than you seem, and smarter than you think.",
+  },
+  {
+    author: "The Blue Fairy (Pinocchio)",
+    quote: "Always let your conscience be your guide.",
+  },
+  {
+    author: "Donald Duck",
+    quote: "Happiness is the richest thing we will ever own.",
+  },
+  {
+    author: "Cinderella (Cinderella)",
+    quote:
+      "Just because it's what's done, doesn't mean it's what should be done.",
+  },
+  {
+    author: "Grandmother Willow (Pocahontas)",
+    quote: "Sometimes the right path is not the easiest one.",
+  },
+  {
+    author: "Remy (Ratatouille)",
+    quote: "The only thing predictable about life is its unpredictability.",
+  },
+  {
+    author: "Peter Pan (Peter Pan)",
+    quote: "Now, think of the happiest things. It's the same as having wings.",
+  },
+  {
+    author: "Walt Disney",
+    quote:
+      "All our dreams can come true, if we have the courage to pursue them.",
+  },
+];
