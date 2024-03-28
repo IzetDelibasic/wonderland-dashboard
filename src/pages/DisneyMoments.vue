4d781712aaed7f23d8ed8d91379ca330521ce992
Izet Delibasic <izetdelibasic55@gmail.com>
2024-03-28 01:01:38 +0100

    Creating functionality for displaying one quote

--- src/pages/DisneyMoments.vue
+++ src/pages/DisneyMoments.vue
@@ -24,9 +24,9 @@
       <!-- Quotes -->
       <div class="mt-8 px-4">
         <h4 class="text-2xl font-bold mb-4">Disney Quotes</h4>
-        <div v-for="(quote, index) in disneyQuotes" :key="index" class="mb-6">
-          <p class="text-lg font-semibold">{{ quote.quote }}</p>
-          <p class="text-gray-400">{{ quote.author }}</p>
+        <div class="">
+          <p class="text-lg font-semibold">{{ randomQuote.quote }}</p>
+          <p class="text-gray-400">{{ randomQuote.author }}</p>
         </div>
       </div>
     </div>
@@ -40,10 +40,20 @@ export default {
   data() {
     return {
       disneyQuotes: disneyQuotes,
+      randomQuote: {},
     };
   },
+  mounted() {
+    this.getRandomQuote();
+  },
   methods: {
-    goToStartPage() {},
+    goToStartPage() {
+      this.$router.push("/");
+    },
+    getRandomQuote() {
+      const randomIndex = Math.floor(Math.random() * this.disneyQuotes.length);
+      this.randomQuote = this.disneyQuotes[randomIndex];
+    },
   },
 };
 </script>
